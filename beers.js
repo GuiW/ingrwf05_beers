eaa18d8d6c8ca1a9b251a4219907449df57cf00e
Guillaume Wilmotte <ingrwf@cpg-429384.cepegra2008.lan>
2017-06-23 16:33:28 +0200

    Ajout de commentaires

--- beers.js
+++ beers.js
@@ -1,4 +1,4 @@
-
+//Simulation de récupération de données en dehors de l'app
  var allBeersApi = [{ //get.JSON
      marque: "Jupiler",
      type: "Pils"
@@ -12,28 +12,34 @@
      type: "Blonde"
  }];
  
+// ************************************************************************** //
 
+//Component beer-item
   var beerItem = Vue.extend({
         template: '#beer-item',
         props: ['infobeer']
 });
 
+//Component beer-list
  var beerList = Vue.extend({
         template: '#beer-list',
+      //Le component beer-list reçoit le compoment beer-item
         components: {
             'beer-item': beerItem
         },
         props: ['collection']
 });
 
+// ************************************************************************** //
 
-
+//Application principale
 var beer = new Vue({
     el: "#app",
     data: {
         site: "Beers",
         allBeers: allBeersApi
     },
+  //L'application principale reçoit le compoment beer-list
     components: {
             'beer-list': beerList
         }
